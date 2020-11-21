dada2be1fa6d5487a518fbb8bffb7dfb636a2702
MacBookPro <macbookpro@Oswaldos-MacBook-Pro.local>
2020-11-20 22:16:59 -0600

    Feat: departments detail

--- src/components/Department/Department.tsx
+++ src/components/Department/Department.tsx
@@ -1,19 +1,66 @@
 import React from 'react';
 import { IHomes } from '../../containers/DepartmentsList/store/departmentsTypes';
-import { DepartmentContainer } from './Department_Style';
+import * as style from './Department_Style';
 
 interface IProps {
     home: IHomes
 }
 
 const Department = ({ home }: IProps) => {
-    const photos=home.photos;
-    const firstPhoto=photos[0];
+    const photos = home.photos;
+    const firstPhoto = photos[0];
+    console.log(home);
+
+    // Create our number formatter.
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0,
+        //maximumFractionDigits: 0,
+    });
+
     return (
-        <DepartmentContainer background={firstPhoto}>
-                hola
-  
-        </DepartmentContainer>
+        <style.DepartmentContainer background={firstPhoto}>
+            <style.Price>
+                {formatter.format(home.price).substring(0, formatter.format(home.price).length - 3)}
+            </style.Price>
+
+            {home.is_homie_exclusive &&
+                <style.Exclusive>
+                    <span>
+                        Exclusivo de Homie
+                </span>
+
+                </style.Exclusive>
+            }
+
+            <style.Description>
+                <style.Address>
+                    {home.abbr_address}
+                </style.Address>
+                <style.Details>
+                <style.Row>
+                <style.Cell>
+                    uno
+                </style.Cell>
+                <style.Cell>
+                    uno
+                </style.Cell>
+                <style.Cell>
+                    uno
+                </style.Cell>
+                <style.Cell>
+                    uno
+                </style.Cell>
+                <style.Cell>
+                    uno
+                </style.Cell>
+                </style.Row>
+                </style.Details>
+            </style.Description>
+
+        </style.DepartmentContainer>
     )
 
 }
--- src/components/Department/Department_Style.ts
+++ src/components/Department/Department_Style.ts
@@ -5,16 +5,70 @@ interface IDepartment {
 }
 
 export const DepartmentContainer= styled.div`
-display: flex;
-flex-direction: row;
-width: 250px;
-height: 221px;
-margin-right: 26px;
-margin-bottom: 17px;
-background-image: url(${(props:IDepartment) => props.background});
-&:first-child {
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+    height: 224px;
     margin-right: 26px;
-    margin-left: 0px;
-}
+    margin-bottom: 17px;
+    background-image: url(${(props:IDepartment) => props.background});
+    &:first-child {
+        margin-right: 26px;
+        margin-left: 0px;
+    }
+    position: relative;
+`;
+
+export const Price= styled.div`
+    width: 108px;
+    height: 32px;
+    background-color: white;
+    padding-left: 11px;
+    color: #ff1555;
+    font-size: 16px;
+    font-weight: 900;
+    font-face: "Open Sans", sans-serif;
+`;
+
+export const Exclusive= styled.div`
+    text-align: center;
+    height: 22px;
+    width: 100%;
+    transform: rotate(360deg);
+    background-color: rgba(255, 3, 100, 0.49);
+    margin-top: 100px;
+    span {
+        color: white;
+        font-weight: 900;
+    }
+    `;
+
+export const Description= styled.div`
+    background-color: white;
+    height:70px;
+    font-weight: bold;
+    position: absolute;
+    bottom: 0;
+    width: 100%
+`;
+
+export const Address= styled.div`
 
 `;
+
+export const Details= styled.div`
+    width: 100%;
+    height:50px;
+    bottom: 0;
+    display: table;
+`;
+
+export const Row= styled.div`
+    display: table-row;
+    `;
+
+export const Cell= styled.div`
+    display: table-cell;
+    padding: 12px;
+    background: #ddd;
+    `;
